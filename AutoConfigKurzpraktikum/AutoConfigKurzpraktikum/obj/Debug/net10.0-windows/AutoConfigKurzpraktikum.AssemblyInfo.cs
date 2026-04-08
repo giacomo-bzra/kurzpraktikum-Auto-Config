cf0f8ba5260afe84309e7e64d490631283fe70fb
gagamo <giacomo.dimitri@bwz-rappi.ch>
2026-04-08 09:06:08 +0200

    test commit

--- AutoConfigKurzpraktikum/AutoConfigKurzpraktikum/obj/Debug/net10.0-windows/AutoConfigKurzpraktikum.AssemblyInfo.cs
+++ AutoConfigKurzpraktikum/AutoConfigKurzpraktikum/obj/Debug/net10.0-windows/AutoConfigKurzpraktikum.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoConfigKurzpraktikum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d0c40cd61ee40b09a3823286d7634d8536fd975")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9626a9a1bd7b167dd1f02bc4c4f7f1c28fec151")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoConfigKurzpraktikum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoConfigKurzpraktikum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

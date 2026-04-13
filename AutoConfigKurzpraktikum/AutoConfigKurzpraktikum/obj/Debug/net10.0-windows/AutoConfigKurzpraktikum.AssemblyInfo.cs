cf9e247d2d649d4daea7fe386c9cf9b65506e642
gagamo <giacomo.dimitri@bwz-rappi.ch>
2026-04-13 13:29:13 +0200

    auto config modifications

--- AutoConfigKurzpraktikum/AutoConfigKurzpraktikum/obj/Debug/net10.0-windows/AutoConfigKurzpraktikum.AssemblyInfo.cs
+++ AutoConfigKurzpraktikum/AutoConfigKurzpraktikum/obj/Debug/net10.0-windows/AutoConfigKurzpraktikum.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoConfigKurzpraktikum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0859a69c0f12aa4f6e761de48b8523cf66b90816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f55d32acf17f6dba444625eb2e9b70a75cd7384b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoConfigKurzpraktikum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoConfigKurzpraktikum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

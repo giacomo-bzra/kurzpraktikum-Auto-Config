1872593f6f073cb8c48e842bf7a12db63ab241e9
gagamo <giacomo.dimitri@bwz-rappi.ch>
2026-04-14 10:19:10 +0200

    final commit vo first window logistics

--- AutoConfigKurzpraktikum/AutoConfigKurzpraktikum/Pages/MainWindow.xaml.cs
+++ AutoConfigKurzpraktikum/AutoConfigKurzpraktikum/Pages/MainWindow.xaml.cs
@@ -8,17 +8,44 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AutoConfigKurzpraktikum.Models;
+using AutoConfigKurzpraktikum.Service;
 
 namespace AutoConfigKurzpraktikum;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
+    public List<Car> AvailableCars { get; set; }
+    private int _currentCarIndex = 0;
+    public Car CurrentCar => AvailableCars[_currentCarIndex];
+    
     public MainWindow()
     {
         InitializeComponent();
+        AvailableCars = new List<Car>
+        {
+            BaseCar.CreateDefault(),
+            BaseCar.CreateRustyBase()
+        };
+        
+        this.DataContext = CurrentCar;
+        Update();
+    }
+
+    public void Button_Left(object sender, RoutedEventArgs e)
+    {
+        _currentCarIndex = (_currentCarIndex + 1) % AvailableCars.Count;
+        Update();
+    }
+
+    public void Button_Right(object sender, RoutedEventArgs e)
+    {
+        _currentCarIndex--;
+        if (_currentCarIndex < 0)
+        {
+            _currentCarIndex = AvailableCars.Count - 1;
+        }
+        Update();
     }
     
     public void Button_select(object sender, RoutedEventArgs e)
@@ -27,6 +54,11 @@ public partial class MainWindow : Window
             tuneWindow.Show();
             this.Close();
     }
-    
-    
+
+    private void Update()
+    {
+        placeholderLabel.Text = CurrentCar.Brand;
+
+        count.Content = $"{_currentCarIndex + 1} / {AvailableCars.Count}";
+    }
 }
--- AutoConfigKurzpraktikum/AutoConfigKurzpraktikum/obj/Debug/net10.0-windows/AutoConfigKurzpraktikum.AssemblyInfo.cs
+++ AutoConfigKurzpraktikum/AutoConfigKurzpraktikum/obj/Debug/net10.0-windows/AutoConfigKurzpraktikum.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoConfigKurzpraktikum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9e247d2d649d4daea7fe386c9cf9b65506e642")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40bf5ed944ace7496fa1513cdaf4d9cdfb8b855f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoConfigKurzpraktikum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoConfigKurzpraktikum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

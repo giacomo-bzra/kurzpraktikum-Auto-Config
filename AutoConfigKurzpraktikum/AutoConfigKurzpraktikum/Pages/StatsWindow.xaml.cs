7a123ccf9d1f7ecc780f632fdbab216e029d532b
sami.bouddat@bwz-rappi.ch <sami.bouddat@bwz-rappi.ch>
2026-04-15 09:45:50 +0200

    Stats display

--- AutoConfigKurzpraktikum/AutoConfigKurzpraktikum/Pages/StatsWindow.xaml.cs
+++ AutoConfigKurzpraktikum/AutoConfigKurzpraktikum/Pages/StatsWindow.xaml.cs
@@ -7,30 +7,48 @@ namespace AutoConfigKurzpraktikum.Pages;
 
 public partial class StatsWindow : Window
 {
-    public StatsWindow(Tire selectedTire, Brake selectedBrake, Engine selectedEngine, Frontspoiler selectedFrontspoiler, Heckspoiler selectedHeckspoiler, Rimm selectedRimm)
+    private Tire _tire;
+    private Brake _brake;
+    private Engine _engine;
+    private Frontspoiler _frontspoiler;
+    private Heckspoiler _heckspoiler;
+    private Rimm _rimm;
+    
+    
+    public StatsWindow(Tire SelectedTire, Brake SelectedBrake, Engine SelectedEngine, Frontspoiler SelectedFrontspoiler, Heckspoiler SelectedHeckspoiler, Rimm SelectedRimm)
     {
         InitializeComponent();
-        Progresses();
+        _tire = SelectedTire;
+        _brake = SelectedBrake;
+        _engine = SelectedEngine;
+        _frontspoiler = SelectedFrontspoiler;
+        _heckspoiler = SelectedHeckspoiler;
+        _rimm =  SelectedRimm;
+
+        Progress();
     }
 
-
-
-    public void Button_Done(object sender, RoutedEventArgs e) {
+    private void Button_Done(object sender, RoutedEventArgs e)
+    {
         this.Close();
     }
 
-    private int speed = 300;
-    private int Brake = 220;
-    private int Grip = 700;
-    private int Weight = 800;
-    private int Cost = 500;
-    
-    private void Progresses()
+    private void Progress()
     {
-        SpeedBar.Value = speed;
-        BrakeBar.Value = Brake;
-        GripBar.Value = Grip;
-        WeightBar.Value = Weight;
+        double totalWeight = _tire.Weight + _brake.Weight + _engine.Weight + _frontspoiler.Weight + _heckspoiler.Weight + _rimm.Weight;
+        WeightBar.Value = totalWeight;
+        
+        double totalCost = _tire.Price + _brake.Price + _engine.Price + _frontspoiler.Price + _heckspoiler.Price + _rimm.Price;
+        LblPrice.Content = $"{totalCost}CHF";
+        
+        double totalDownforce = _frontspoiler.Downforce + _heckspoiler.Downforce;
+        LblDownforce.Content = $"{totalDownforce}N";
+        
+        LblFuelconsumption.Content = _engine.FuelConsumption;
+        LblType.Content = _tire.TireType;
+
+        SpeedBar.Value = _engine.HorsePower;
+        BrakeBar.Value = _brake.Brakeforce;
+        GripBar.Value = _tire.Grip;
     }
-    
-    }
+}
